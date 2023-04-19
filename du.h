222e7961ef4a5b15de45e66cc0757b387753764e
crkur <ckurdelak20@georgefox.edu>
2023-04-19 12:11:52 -0700

    updated documentation

--- du.h
+++ du.h
@@ -13,6 +13,7 @@
  * @param dir_name the filepath of the directory
  * @param opt_all true if all the files in the directory are to have their space usage printed
  * @param opt_bytes true if the space usage is to be printed in bytes instead of 1024 byte units
+ * @return the total space taken by the specified directory in bytes
  */
 unsigned long process_dir(char *dir_name, bool opt_all, bool opt_bytes);
 
